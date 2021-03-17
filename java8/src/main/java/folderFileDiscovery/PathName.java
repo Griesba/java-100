a6c88bb7e5846f5cae99f83afc40be5135de7aa4
griesba <griesba.pat@hotmail.com>
2021-03-17 22:53:41 +0100

    File discovery

--- java8/src/main/java/folderFileDiscovery/PathName.java
+++ java8/src/main/java/folderFileDiscovery/PathName.java
@@ -4,24 +4,36 @@ import java.io.File;
 
 public class PathName {
 
-    public static void listDirectory(String path){
+    public static boolean listDirectory(String path, String fileName){
         File file = new File(path);
         if (file != null) {
             File[] files = file.listFiles();
             if (files != null && files.length > 0) {
                 for (File file1 : files){
                     if (file1.isDirectory()) {
-                        System.out.println("D-" +file1.getName());
-                        listDirectory(file1.getAbsolutePath());
+                        //System.out.println("D-" +file1.getName());
+                        if (listDirectory(file1.getAbsolutePath(), fileName)) {
+                            return true;
+                        }
                     } else {
+                        if (file1.getName().equals(fileName) || file1.getName().startsWith(fileName + ".")) {
+                            System.out.println("File found in directory "+ file.getName());
+                            return true;
+                        }
                         System.out.println("F-" + file1.getName() + " ("+ file1.length() +" bytes)");
                     }
                 }
             }
         }
+        return false;
     }
 
     public static void main(String[] args) {
-        listDirectory(args[0]);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid input. Usage: <directory name> <file name>");
+        }
+        if (!listDirectory(args[0], args[1])) {
+            System.out.println(args[1] + " not found");
+        }
     }
 }
