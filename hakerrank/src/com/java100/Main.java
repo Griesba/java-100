cfbe93948442ee098aa6a1998f8ca1be65b6576d
griesba <griesba.pat@hotmail.com>
2021-08-24 23:12:05 +0200

    Hourglass test (hackerrank)

---
+++ hakerrank/src/com/java100/Main.java
@@ -0,0 +1,8 @@
+package com.java100;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ hakerrank/src/com/java100/hourglass/HourGlass.java
@@ -0,0 +1,51 @@
+package com.java100.hourglass;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class HourGlass {
+    public static void main(String[] args) throws IOException {
+
+        InputStream is = HourGlass.class.getClass().getClassLoader().getResourceAsStream("com/java100/hourglass/hourglass_in.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int height = arr.size();
+        int max= 0, sum = 0;
+
+
+        for(int j = 0; j < (arr.size() - 3); j++) {
+            for(int i = 0; i < (arr.get(j).size() - 3); i++) {
+                sum +=arr.get(j).get(i) + arr.get(j).get(i+1) + arr.get(j).get(i+2);
+                sum +=arr.get(j+1).get(i+1);
+                sum +=arr.get(j+2).get(i) + arr.get(j+2).get(i+1) + arr.get(j+2).get(i+2);
+                if(sum > max) {
+                    max = sum;
+                }
+                sum = 0;
+            }
+        }
+
+        System.out.println(max);
+
+        bufferedReader.close();
+    }
+}
