4b208ece826beb447e5223fbe0ee51f8092f1fd0
griesba <griesba.pat@hotmail.com>
2022-07-12 00:25:35 +0200

    [Thread] example of deadlock problem

---
+++ Threads/src/test/com/griesba/java100/threads/deadlocks/PolitePersonTest.java
@@ -0,0 +1,29 @@
+package com.griesba.java100.threads.deadlocks;
+
+
+import com.griesba.java100.threads.threads.deadlocks.PolitePerson;
+import org.junit.jupiter.api.Test;
+
+public class PolitePersonTest {
+
+    @Test
+    public void shouldSayHelloAndHelloBack() {
+        PolitePerson jane = new PolitePerson("jana");
+        PolitePerson john = new PolitePerson("john");
+
+        jane.sayHello(john);
+        john.sayHello(jane);
+    }
+
+    @Test
+    public void shouldSayHelloAndDeadlock() {
+        PolitePerson jane = new PolitePerson("jana");
+        PolitePerson john = new PolitePerson("john");
+        // this is deadlock prone as in most case the thread won't run the sayHelloBack method
+        // because it is suspended by the other thread
+        new Thread(() -> jane.sayHello(john)).start();
+
+        new Thread(() -> john.sayHello(jane)).start();
+    }
+
+}
