989474da02d04fe9d27fccd74871881eedd8593a
griesba <griesba.pat@hotmail.com>
2023-02-27 10:48:41 +0100

    Design pattern : Composite pattern

---
+++ desing-pattern/readme.md
@@ -0,0 +1,9 @@
+#Composite pattern
+pattern structurel.
+Permet de créer une structure arborescente dans laquelle un groupe d'objet est traité comme un seul objet.
+Ce patron permet de proposer une interface qui permet de traite le corp comme les feuilles da l'arbre.
+
+Ce patron permet de concevoir une structure arborescente, par exemple une représentation d'un dossier, ses sous-dossier et leurs fichiers.
+
+Ce patron composite propose une sctructure récursive permettant d'implementer avec la même interface logicielle 
+sur les feuilles et les composites afin qu'ils soient manipulés de la même manière
---
+++ desing-pattern/src/com/griesba/java100/composite/CompositeGraphic.java
@@ -0,0 +1,62 @@
+package com.griesba.java100.composite;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface Graphic {
+    void print();
+}
+
+class Eclipse implements Graphic {
+
+    @Override
+    public void print() {
+        System.out.println("Eclipse");
+    }
+}
+
+public class CompositeGraphic implements Graphic {
+
+    private List<Graphic> graphicList = new ArrayList<>();
+
+    @Override
+    public void print() {
+        for (Graphic graphic:
+             graphicList) {
+            graphic.print();
+        }
+    }
+
+    public void add(Graphic graphic) {
+        graphicList.add(graphic);
+    }
+
+    public void remove(Graphic graphic) {
+        graphicList.remove(graphic);
+    }
+}
+
+class Programme {
+    public static void main(String[] args) {
+        Eclipse eclipse1 = new Eclipse();
+        Eclipse eclipse2 = new Eclipse();
+        Eclipse eclipse3 = new Eclipse();
+        Eclipse eclipse4 = new Eclipse();
+
+        CompositeGraphic graphic = new CompositeGraphic();
+        CompositeGraphic graphic1 = new CompositeGraphic();
+        CompositeGraphic graphic2 = new CompositeGraphic();
+
+        graphic1.add(eclipse1);
+        graphic1.add(eclipse2);
+        graphic1.add(eclipse3);
+
+        graphic2.add(eclipse4);
+
+        graphic.add(graphic2);
+        graphic.add(graphic1);
+
+        graphic.print();
+    }
+}
